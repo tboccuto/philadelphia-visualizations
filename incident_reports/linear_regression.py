d1ebbf752c88ed2fcf8ba7597e599f125cc91e1f
tboccuto <59341700+tboccuto@users.noreply.github.com>
2022-12-23 18:05:13 -0500

    cleaning up

--- incident_reports/linear_regression.py
+++ incident_reports/linear_regression.py
@@ -7,25 +7,37 @@ import sklearn
 import matplotlib.pyplot as plt
 from matplotlib import animation, rc
 
+class LinearRegression:
+  def __init__(self, X, y, plotting=True):
+    self.X = X
+    self.y = y
+    self.preds = []
+    self.costs = []
+    self.thetas = []
 
-def gradient_descent(x, y, m, theta,alpha,verbose=True):
-  costs, thetas, preds = [], [] ,[]  #used for plotting
-  conv,idx = 1,0
-  costs.append(1e10)    
-  while conv:
-    pred= np.dot(x, theta)   #y_prediction
-    preds.append(pred)
-    error = pred - y          
-    cost = 1/(2*m) * np.dot(error.T, error) 
-    if verbose: print(f'here is cost {cost} at iteration {idx}')
-    costs.append(cost)
-    theta = theta - (alpha * (1/m)) * np.dot(x.T,error)
-    thetas.append(theta)
-    if costs[idx]-costs[idx+1] < 1e-9:
-        conv = 0
-    idx+=1
-  costs.pop(0)   
-  return costs, thetas, preds
+  def feature_scaling(self, X, Y):
+    return (X - X.mean())/X.std(), (Y - Y.mean())/Y.std()
+
+  def gradient_descent(self, x, y, theta,alpha,fs=True,verbose=False):
+    if fs:
+      x, y = self.feature_scaling(x, y)
+    if len(x.shape) == 1:
+      x = np.c_[np.ones(x.shape[0]),x]
+    self.X = x #update attr
+    conv,idx = 1,0
+    self.costs.append(1e10)
+    while conv:
+      self.preds.append(np.dot(x, theta)) #y_prediction
+      error = self.preds[idx] - y
+      self.costs.append(1/(2*y.size) * np.dot(error.T, error))
+      theta = theta - (alpha * (1/y.size)) * np.dot(x.T,error)
+      self.thetas.append(theta)
+      if verbose: print(f'{idx}[cost:{self.costs[idx]} preds: {self.preds[idx]} thetas: {self.thetas[idx]}]')
+      if self.costs[idx]-self.costs[idx+1] < 1e-9:
+          conv = 0
+      idx+=1
+    self.costs.pop(0)
+    print(theta[0], theta[1])
 
 
 
